df6b087cf71d08d443dfd3ad3535c10412fce04b
kewang <cpckewang@gmail.com>
2015-09-13 17:36:31 +0800

    Maven support

---
+++ src/main/java/tw/kewang/hbase/App.java
@@ -0,0 +1,13 @@
+package tw.kewang.hbase;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
