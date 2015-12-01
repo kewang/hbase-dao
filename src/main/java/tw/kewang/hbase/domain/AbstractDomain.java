9000270766a717faa200a7d58a90a57608617789
kewang <cpckewang@gmail.com>
2015-12-02 00:41:24 +0800

    Refactor

--- src/main/java/tw/kewang/hbase/domain/AbstractDomain.java
+++ src/main/java/tw/kewang/hbase/domain/AbstractDomain.java
@@ -14,43 +14,44 @@ public abstract class AbstractDomain {
 	private ColumnQualifier qualifier;
 
 	public String getRowkey() {
-		StringBuffer sb = new StringBuffer();
-
 		Class<?> clazz = getClass();
 
 		Rowkey rowkey = clazz.getAnnotation(Rowkey.class);
 
 		if (rowkey != null) {
-			Pattern pattern = Pattern.compile(REGEX);
-
-			String rowkeyPattern = rowkey.value();
-
-			Matcher matcher = pattern.matcher(rowkeyPattern);
-
-			while (matcher.find()) {
-				for (Field field : clazz.getDeclaredFields()) {
-					field.setAccessible(true);
-
-					Component component = field.getAnnotation(Component.class);
-
-					if (component.name().equals(matcher.group(1))) {
-						try {
-							String s = (String) field.get(this);
-
-							matcher.appendReplacement(sb, s);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
-				}
-			}
-
-			matcher.appendTail(sb);
-
-			return sb.toString();
+			return buildRowkey(clazz, rowkey);
 		}
 
 		return null;
+	}
+
+	private String buildRowkey(Class<?> clazz, Rowkey rowkey) {
+		Pattern pattern = Pattern.compile(REGEX);
+		String rowkeyPattern = rowkey.value();
+		Matcher matcher = pattern.matcher(rowkeyPattern);
+		StringBuffer sb = new StringBuffer();
+
+		while (matcher.find()) {
+			for (Field field : clazz.getDeclaredFields()) {
+				field.setAccessible(true);
+
+				Component component = field.getAnnotation(Component.class);
+
+				if (component.name().equals(matcher.group(1))) {
+					try {
+						String value = (String) field.get(this);
+
+						matcher.appendReplacement(sb, value);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+
+		matcher.appendTail(sb);
+
+		return sb.toString();
 	}
 
 	public ColumnFamily getFamily() {
