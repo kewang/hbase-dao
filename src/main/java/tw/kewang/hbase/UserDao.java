e8594da0b41ddaad361029455a164ba103fe5c58
kewang <cpckewang@gmail.com>
2015-12-06 23:02:07 +0800

    Add more domain

--- src/main/java/tw/kewang/hbase/UserDao.java
+++ src/main/java/tw/kewang/hbase/UserDao.java
@@ -3,6 +3,6 @@ package tw.kewang.hbase;
 import tw.kewang.hbase.annotations.Table;
 import tw.kewang.hbase.dao.AbstractDao;
 
-@Table(name = "User", domains = { User1.class })
+@Table(name = "User", domains = { User1.class, User2.class })
 public class UserDao extends AbstractDao {
 }
