e9299fc9138d26066b72f95a8da4a588992032bd
kewang <cpckewang@gmail.com>
2015-12-12 20:36:00 +0800

    Add more tests

--- src/test/java/tw/kewang/hbase/DaoTest.java
+++ src/test/java/tw/kewang/hbase/DaoTest.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 import tw.kewang.hbase.dao.HBaseSettings;
 
 public class DaoTest extends TestCase {
-	public void testUser1NoFields() {
+	public void testUser1GetByRowkey() {
 		HBaseSettings.setZooKeeperQuorum("localhost");
 
 		UserDao uDao = new UserDao();
@@ -13,5 +13,27 @@ public class DaoTest extends TestCase {
 
 		assertEquals("xyz", u1.getUserId());
 		assertEquals("abc", u1.getAccessToken());
+	}
+
+	public void testUser1GetByRowkeyNotFound() {
+		HBaseSettings.setZooKeeperQuorum("localhost");
+
+		UserDao uDao = new UserDao();
+
+		User1 u1 = (User1) uDao.getByRowkey("xyz_a");
+
+		assertNull(u1);
+	}
+
+	public void testUser1AndUser2GetByRowkey() {
+		HBaseSettings.setZooKeeperQuorum("localhost");
+
+		UserDao uDao = new UserDao();
+
+		User1 u1 = (User1) uDao.getByRowkey("xyz_abc");
+		User2 u2 = (User2) uDao.getByRowkey("xyz_abc_kewang");
+
+		assertNotNull(u1);
+		assertNotNull(u2);
 	}
 }
